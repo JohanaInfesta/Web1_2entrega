2ef3f489b9a23b3593b3932691f6b72e67836c85
Johana Infesta <johana.infesta@gmail.com>
2018-06-06 20:19:33 -0300

    hay que arreglar el GET

--- js/script.js
+++ js/script.js
@@ -4,67 +4,61 @@ fetch("html/foro.html").then(
       function(texto){
         document.querySelector(".container").innerHTML = texto;
       });
-    }
-  );
+  }
+);
 
   /*partial render*/
-  function loadClick(event){
+function loadClick(event){
     // let url = document.querySelectorAll('href'); //hay que ver como hacer que vaya al html designado
-    event.preventDefault();
-    fetch('html/anime.html').then( function(response){
-      console.log("ok");
-      console.log(response);
-      response.text().then(t=> document.querySelector(".container").innerHTML = t);
-    });
+  event.preventDefault();
+  fetch('html/anime.html').then( function(response){
+    console.log("ok");
+    console.log(response);
+    response.text().then(t=> document.querySelector(".container").innerHTML = t);
+  });
+}
+
+let jsloads = document.querySelectorAll(".nav-item");
+jsloads.forEach(e=> e.addEventListener("click", loadClick));
+
+let url = 'http://web-unicen.herokuapp.com/api/groups/400/probando_grupo40';
+
+function enviarComentario(){
+  event.preventDefault();
+  let comentarios = {
+    'nombre' : $(".js-input-nombre").val(),
+    'comentario' : $(".js-input-comentario").val(),
   }
-
-  let jsloads = document.querySelectorAll(".nav-item");
-  jsloads.forEach(e=> e.addEventListener("click", loadClick));
-
-
-  // let groupID = 400;
-  // let url = 'http://web-unicen.herokuapp.com/api/groups/';
-
-  function enviarComentario(){
-    event.preventDefault();
-    let comentarios = {
-      'nombre' : $(".js-input-nombre").val(),
-      'comentario' : $(".js-input-comentario").val(),
-    }
-    // let comentario = {
-    //   "nombre" : document.querySelector(".js-input-nombre").value,
-    //   "comentario" :document.querySelector(".js-input-comentario").value,
-    // }
-    let info = {
-      thing: comentarios //puede ser un objeto JSON!
-    };
-    if (comentarios) {
-      fetch('http://web-unicen.herokuapp.com/api/groups/400/probando_grupo40',{
-        method: 'POST',
-        mode: "cors",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(info),
-      }).then( r => console.log(r))
-    }
+  let info = {
+    thing: comentarios //puede ser un objeto JSON!
+  };
+  if (comentarios) {
+    fetch(url,{
+      method: 'POST',
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(info),
+    }).then( r => console.log(r))
   }
-  // function loadComentario(){
-  //   event.preventDefault();
-  //   let contenedor = document.querySelector("#template");
-  //   contenedor.innerHTML = "Cargando....";
-  //   fetch(url + groupID +'/' + 'probando_grupo40')
-  //   .then(r => r.JSON())
-  //   .then(JSON => Mostrar(contenedor, JSON)
-  //   .catch(error => contenedor.innerHTML))
-  // }
-  //
-  // function Mostrar(contenedor, JSON){
-  //   for (var i = 0; i < JSON.probando_grupo40.length; i++) {
-  //     let nombre = JSON.probando_grupo40[i].thing.nombre;
-  //     let comentario = JSON.probando_grupo40[i].thing.comentario;
-  //     let html = "<tr>";
-  //     html += "<td>" + nombre + "</td>";
-  //     html += "<td>" + comentario + "</td>";
-  //   }
-  //   html += "</tr>";
-  //   contenedor.innerHTML = html;
-  // }
+}
+
+function resetComentarios(){
+  let contenidoComentario = document.querySelector('#template');
+  console.log(contenidoComentario);
+  fetch(url,{
+    method :'GET'
+  }).then(r => r.JSON())
+    .then(JSON => MostrarComentarios(contenidoComentario, JSON))
+    .catch(error => contenidoComentario.innerHTML)
+    console.log(contenidoComentario);
+}
+function MostrarComentarios(contenido, JSON){
+  let html = " "
+  for (var i = 0; i < JSON.probando_grupo40.length; i++) {
+    html = "<tr id= '" + JSON.probando_grupo40[i]._id + "'>";
+    html += "<td>" + JSON.probando_grupo40[i].thing['nombre'] + "</td>";
+    html += "<td>" + JSON.probando_grupo40[i].thing['comentario'] + "</td></tr>";
+  }
+console.log(html);
+  contenido.innerHTML = html;
+}
